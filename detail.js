57ddfc9aee4cf0870782ebec12a9b11eee4b012f
zombiiee-Dev <phung2012lk@gmail.com>
2023-07-19 23:38:35 +0700

    them

--- detail.js
+++ detail.js
@@ -103,6 +103,17 @@ const renderDetail = async() =>{
     }
 }
 renderDetail();
+// gameData.data.categories.forEach(category => {
+//     const tag = document.createElement("div");
+//     tag.classList.add("tag");
+//     const link = document.createElement("a");
+//     link.href = category;
+//     link.textContent = category;
+//     tag.appendChild(link);
+//     // Thêm tag vào vị trí mong muốn trong DOM
+//     // Ví dụ: document.querySelector(".tag-container").appendChild(tag);
+//   });
+  
 const getGenresList = async() =>{
   const url_genres = `https://steam-api-dot-cs-platform-306304.et.r.appspot.com/genres?page=1&limit=7`
   try {
